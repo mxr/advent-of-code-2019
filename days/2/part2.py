16b7b6242097f4956e4d8b6ad048986a5083840a
Max R <mxr@users.noreply.github.com>
2019-12-02 08:35:25 -0500

    Add day 2 solutions (#2)

---
+++ days/2/part2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from part1 import override_inputs
+from part1 import read_inputs
+from part1 import run_until_termination
+
+
+def main() -> int:
+    inputs = read_inputs()
+
+    for noun in range(100):
+        for verb in range(100):
+            overridden = override_inputs(inputs, noun, verb)
+
+            if run_until_termination(overridden) == 19690720:
+                print(100 * noun + verb)
+                return 0
+
+    # should not be reached
+    return 1
+
+
+if __name__ == "__main__":
+    exit(main())
