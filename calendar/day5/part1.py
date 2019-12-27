f576d6cbf44a896234836c20836030f1fa36dfaa
Max R <mxr@users.noreply.github.com>
2019-12-27 11:06:35 -0800

    Add day 5 solutions (#6)

---
+++ calendar/day5/part1.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import fileinput
+from contextlib import closing
+from typing import List
+from typing import Tuple
+
+
+def read_inputs() -> Tuple[int, ...]:
+    with closing(fileinput.input()) as f:
+        return tuple(int(x) for x in f.readline().split(","))
+
+
+def run_until_termination(inputs: List[int]) -> int:
+    i = 0
+    while True:
+        op = int(str(inputs[i])[-2:])
+        params = str(inputs[i])[:-2].zfill(3)
+
+        if op == 1:
+            inputs[inputs[i + 3]] = _value(inputs, i + 1, params[-1]) + _value(
+                inputs, i + 2, params[-2]
+            )
+            i += 4
+        elif op == 2:
+            inputs[inputs[i + 3]] = _value(inputs, i + 1, params[-1]) * _value(
+                inputs, i + 2, params[-2]
+            )
+            i += 4
+        elif op == 3:
+            inputs[inputs[i + 1]] = int(fileinput.input().readline())
+            i += 2
+        elif op == 4:
+            print(_value(inputs, i + 1, params[-1]))
+            i += 2
+        else:
+            assert op == 99
+            return inputs[0]
+
+
+def _value(inputs: List[int], i: int, mode: str) -> int:
+    return inputs[inputs[i]] if mode == "0" else inputs[i]
+
+
+def main() -> int:
+    inputs = list(read_inputs())
+
+    run_until_termination(inputs)
+
+    return 0
+
+
+if __name__ == "__main__":
+    exit(main())
