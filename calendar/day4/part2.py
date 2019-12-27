4d876beb74e27a975d0a6d600c5f9756b9f75f5a
Max Rozentsveyg <maxr@outlook.com>
2019-12-26 23:26:38 -0800

    Add day4 part2 solution

--- calendar/day4/part2.py
+++ calendar/day4/part2.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 import fileinput
+from collections import Counter
 from itertools import tee
 
 
 def matches(num: int) -> bool:
-    consecutive_equal = False
+    counts = Counter(str(num))
 
     digits1, digits2 = tee(str(num))
     next(digits2)
+
+    consecutive_digits = set()
     for digit1, digit2 in zip(digits1, digits2):
         if int(digit2) < int(digit1):
             return False
-        consecutive_equal |= digit1 == digit2
+        if digit1 == digit2:
+            consecutive_digits.add(digit1)
 
-    return consecutive_equal
+    return any(counts[d] == 2 for d in consecutive_digits)
 
 
 def main() -> int:
